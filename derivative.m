9b51ba0d8222642940ab959f8b84ce68c2898acf
JavierGatica <javier.gatica@uc.cl>
2023-08-13 13:45:23 -0400

    test2 error fixed

--- derivative.m
+++ derivative.m
@@ -4,15 +4,18 @@ function delta_i = derivative(i, S, means, stdevs, delta)
 
     % first integral
     compset = setdiff(1:k, union(S, i));
-
-    f = @(W) arrayfun(@(w) prod(normcdf(w - delta, means(S), stdevs(S))) * prod(normcdf(w, means(compset), stdevs(compset))) * normpdf(w, means(i), stdevs(i)), W);
+    f = @(w) prod(normcdf(w - delta, means(S), stdevs(S))) * prod(normcdf(w, means(compset), stdevs(compset))) * ...
+                                                                                        normpdf(w, means(i), stdevs(i));
+    f = @(W) arrayfun(f, W);
 
     delta_i = integral(f, -Inf, Inf);
 
     % sum of integrals
     for l = compset
         compsetl = setdiff(compset, l);
-        f = @(W) arrayfun(@(w) prod(normcdf(w - delta, means(S), stdevs(S))) * prod(normcdf(w, means(compsetl), stdevs(compsetl))) * (normcdf(w, means(i), stdevs(i)) - normcdf(w - delta, means(i), stdevs(i))) * normpdf(w, means(l), stdevs(l)), W);
+        f = @(w) prod(normcdf(w - delta, means(S), stdevs(S))) * prod(normcdf(w, means(compsetl), stdevs(compsetl))) * ...
+            (normcdf(w, means(i), stdevs(i)) - normcdf(w - delta, means(i), stdevs(i))) * normpdf(w, means(l), stdevs(l));
+        f = @(W) arrayfun(f, W);
         delta_i = delta_i + integral(f, -Inf, Inf);
     end
 end 
--- test2.m
+++ test2.m
@@ -1,8 +1,8 @@
 means = (1:10) * 0.1;
 stdevs = ones(1,10) * 0.1;
-delta = 0.15;
+delta = 0;
 
-pPGI = derivative(i, [], means, stdevs, delta);
+pPGI = derivative(1, [], means, stdevs, delta);
 for i = 2:10
     pPGI = pPGI + derivative(i, 1:(i - 1), means, stdevs, delta);
 end
